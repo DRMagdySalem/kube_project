dd3af59191b3e924fedd479db99c7c9a07b4c14b
EC2 Default User <ec2-user@ip-172-31-37-160.ec2.internal>
2022-11-11 13:38:12 +0000

    before circleci

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=kube_proj
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy kube-proj --image=magdysalemm/kube_proj:v1.0.0
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get deploy,rs,svc,pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/kube-proj-7db945c845-dqqhq --address 0.0.0.0 8000:80
